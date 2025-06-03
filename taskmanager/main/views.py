b80ec03977f68b4e6f144fb3dbcf4a70e9b84c5c
Aleksander_hub <oshepkov77@gmail.com>
2025-06-03 08:53:21 +0300

    ok1

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Task
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+    
+def create(request):
+    return render(request, 'main/create.html')
+
+
